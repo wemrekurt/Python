5c6ddf8600f003c2198ff77d152a4f14827166f8
wemrekurt <emre.kurt@bil.omu.edu.tr>
2015-12-04 21:07:19 +0200

    ParChecker fonksiyonu yazıldı

--- Stack.py
+++ Stack.py
@@ -17,3 +17,34 @@ class Stack:
 	def size(self):
 		return len(self.items)
 
+def parChecker(symbolString):
+	s	= Stack()
+	balanced = True
+	index =0
+
+	while index < len(symbolString) and balanced:
+		symbol = symbolString[index]
+		if symbol== "(":
+			s.push(symbol)
+		else:
+			if s.isEmpty():
+				balanced=False
+			else:
+				s.pop()
+
+		index=index+1
+
+	if balanced and s.isEmpty():
+		return True
+	else:
+		return s.size()
+
+x= parChecker('(()(())')
+
+if x==True:
+	print "Duzenli parantez"
+else:
+	if s.size()>0:
+		print "Acilmis ancak kapanmamis "+str(x)+" parantez var"
+	else:
+		print "Kapanmis ancak acilmamis parantez var"
