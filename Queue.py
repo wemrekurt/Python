de7d3496bf6a8189d345f3025aea6f698bb24709
wemrekurt <emre.kurt@bil.omu.edu.tr>
2016-01-06 15:09:27 +0200

    kuyruk güncel

--- Queue.py
+++ Queue.py
@@ -6,10 +6,10 @@ class Queue:
     return self.items == []
 
   def enqueue(self,item):
-    self.items.insert(0,item)
+    self.items.insert(0,item) #.append(item)
 
   def dequeue(self):
-    return self.items.pop()
+    return self.items.pop() #.pop(0)
 
   def size(self):
     return len(self.items)
