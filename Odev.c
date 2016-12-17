0743016c68e0dd83f1ceb457cb9605e7370c93d3
wemrekurt <emre.kurt@bil.omu.edu.tr>
2016-12-17 18:03:51 +0200

    bfs cikarildi

--- Odev.c
+++ Odev.c
@@ -7,6 +7,8 @@ struct Vertex {
   int key;
   char val;
   char color;
+  int distance;
+  int adjSize;
   struct Vertex* pred;
   struct AdjList* adjs;
 };
@@ -33,6 +35,8 @@ struct Vertex* addVertex(struct Graph* graph, int key, char val){
   newNode->key = key;
   newNode->val = val;
   newNode->color = 'w';
+  newNode->distance = 0;
+  newNode->adjSize = 0;
   newNode->pred = NULL;
   newNode->adjs = (struct AdjList*) malloc(graph->size * sizeof(struct AdjList));
 
@@ -53,6 +57,7 @@ void addEdge(struct Graph* graph, int src, int dest){
   for(i = 0; i < graph->size; i++){
     if(graph->array[src].vertex->adjs[i].vertex == NULL){
       graph->array[src].vertex->adjs[i].vertex = graph->array[dest].vertex;
+      graph->array[src].vertex->adjSize = graph->array[src].vertex->adjSize +1;
       break;
     }else{
       // Daha once komsuluk iliskisi kurulmus aynı elemanlar icin tekrar kurulmaması gerekiyor.
@@ -68,10 +73,11 @@ void addEdge(struct Graph* graph, int src, int dest){
     for(i = 0; i < graph->size; i++){
       if(graph->array[dest].vertex->adjs[i].vertex == NULL){
         graph->array[dest].vertex->adjs[i].vertex = graph->array[src].vertex;
+        graph->array[dest].vertex->adjSize = graph->array[dest].vertex->adjSize +1;
         break;
       }
     }
-    printf("%c ve %c arasinda kenar olusturuldu \n", graph->array[src].vertex->val, graph->array[dest].vertex->val);
+    // printf("%c ve %c arasinda kenar olusturuldu \n", graph->array[src].vertex->val, graph->array[dest].vertex->val);
   }else{
     printf("%c ve %c arasinda daha once kenar olusturulmus. O nedenle kenar olusturulmadi. \n", graph->array[src].vertex->val, graph->array[dest].vertex->val);
   }
@@ -93,15 +99,6 @@ struct Graph* createGraph(int size){
   return graph;
 }
 
-// Tüm düğümlerin key-value görebilmek için printVertex fonksiyonu
-void printVertex(struct Graph* graph){
-  int v;
-  for (v = 0; v < graph->size; ++v) {
-    struct Vertex* pCrawl = graph->array[v].vertex;
-    printf("Key: %d Value: %c  \n", v, pCrawl->val);
-  }
-}
-
 // Bir düğümün komşularını listeleyen fonksiyon
 void listNeighbors(struct Graph* graph, int key){
   int i;
@@ -116,112 +113,174 @@ void listNeighbors(struct Graph* graph, int key){
   }
 }
 
-/**
-  Buraya Kadar Graph yapılarını ve fonksiyonlarını gerçekledik. Buradan sonra
-  bfs için gerekli fonksiyonları gerçekleyeceğiz.
+// Tüm düğümlerin key-value görebilmek için printVertex fonksiyonu
+void printVertex(struct Graph* graph){
+  int v;
+  for (v = 0; v < graph->size; ++v) {
+    struct Vertex* pCrawl = graph->array[v].vertex;
+    if(pCrawl->pred != NULL){
+      printf("Key: %d Value: %c Color: %c Distance: %d Pred: %c \n", v, pCrawl->val, pCrawl->color, pCrawl->distance, pCrawl->pred->val);
+    }else{
+      printf("Key: %d Value: %c Color: %c Distance: %d Pred: NULL \n", v, pCrawl->val, pCrawl->color, pCrawl->distance);
+    }
 
-  BFS için bize bir kuyruk yapısı gerek. Temel kuyruğa ekle ve kuyruktan çıkar
-  işlemleri için basitçe kuyruk yapısı gerçekleyeceğiz.
+    listNeighbors(graph, pCrawl->key);
+  }
+}
+
+
+/**
+  Tüm yolları bulmak için recrusive bir fonksiyon kullanacagiz. Bu fonksiyon
+  icinde cikmaz yok oldugunda bir onceki dugumden devam etmesi gerekeceginden
+  bir de stack veri yapisina ihtiyac var.
 */
 
-struct VertList{
-  struct Vertex* vertex;
-};
 
-struct Queue {
+struct Stack {
   int size;
-  struct VertList* elements;
+  int array[];
 };
 
-struct Queue* createQueue(){
-  int len = sizeof(struct VertexList) * sizeof(struct VertexList);
-  struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
-  queue->size = len;
-  queue->elements = (struct VertList*) malloc(len);
+struct Stack* createStack(int size){
+  struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+  stack->size = size;
 
-  printf("uzunluk: %d \n",len);
   int i;
-  // Tüm vertex türündeki elemanları NULL yaparak kuyruğu ilkliyoruz.
-  for (i = 0; i < len; ++i)
-    queue->elements[i].vertex = NULL;
+  // Tum vertex turundeki elemanlari NULL yaparak yigini ilkliyoruz.
+  for (i = 0; i < size; ++i)
+    stack->array[i] = -1;
 
-  return queue;
+  return stack;
+
 }
 
-// Kuyruk elemanlarini ekrana yazmak istiyoruz.
-void printQueue(struct Queue* queue){
+int push(struct Stack* stack, int key){
   int i;
-  for(i=0; i < queue->size; i++){
-    if(queue->elements[i].vertex != NULL){
-      printf("%c - ", queue->elements[i].vertex->val);
+  for(i = 0; i < stack->size; i++){
+    if(stack->array[i] < 0 ){
+      stack->array[i] = key;
+      break;
+    }
+  }
+  return key;
+}
+
+int pull(struct Stack* stack){
+  int i;
+  int j;
+  while(stack->array[i] > -1 && i < stack->size){
+    j = stack->array[i];
+    i++;
+  }
+  stack->array[i-1] = -1;
+  return j;
+}
+
+void printWay(struct Graph* graph, struct Stack* stack){
+  int i = 0;
+  while(stack->array[i] > -1){
+    // printf("%c -->",graph->array[stack->array[i]].vertex->val);
+    printf("%d | ",stack->array[i]);
+    i++;
+  }
+}
+
+void findAllWay(struct Graph* graph, struct Stack* stack, int from, int to){
+  int i = 0;
+  int j;
+  struct Vertex* v = graph->array[from].vertex;
+  v->color = 'g';
+  push(stack, from);
+  for(j = 0; j < v->adjSize; j++){
+    struct Vertex* w = v->adjs[j].vertex;
+    printf("%c (%d) nin komsu: %c (%d) renk %c \n", v->val, v->key, w->val, w->key, w->color);
+
+    if(w->color == 'w'){
+      if(w->key != to){
+        printWay(graph, stack);
+        printf("\n");  printf("\n");
+        findAllWay(graph, stack, w->key, to);
+      }else{
+        // printWay(graph, stack);
+        printf("%c \n", w->val);
+        pull(stack);
+      }
     }else{
-      printf("\n");
-      break;
-    }
-  }
-}
-
-// Kuyruktan eleman cikarma islemi.
-struct Vertex* dequeue(struct Queue* queue){
-  struct Vertex* vert = queue->elements[0].vertex;
-  int i;
-  // Kuyrukta kaydirma islemi.
-  for(i = 0; i < queue->size - 1; i++){
-    queue->elements[i].vertex = queue->elements[i+1].vertex;
-    if(queue->elements[i].vertex == NULL){
-      break;
-    }
-  }
-  return vert;
-}
-
-// Kuyruga eleman ekleme islemi
-struct Vertex* enqueue(struct Queue* queue, struct Vertex* vert){
-  int i;
-  for(i = 0; i < queue->size; i++){
-    if(queue->elements[i].vertex == NULL){
-      queue->elements[i].vertex = vert;
-      break;
+      i++;
     }
   }
 
-  return vert;
-}
 
-// Breath First Search
-// bool bfs(struct Graph* graph, int from, int to){
-//   int i;
-//   for(i = 0; graph -> size; i++){
-//
-//   }
-//   return false;
-// }
+}
 
 int main(){
+  // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+  // A B C D E F G H I J K  L  M  N  0  P  Q  R
   int sizeOfCities = 18;
   struct Graph* graph = createGraph(sizeOfCities);
-
-  for(int i = 0; i < sizeOfCities; i++)
+  struct Stack* stack = createStack(sizeOfCities * sizeOfCities);
+  int i;
+  for(i = 0; i < sizeOfCities; i++)
     addVertex(graph, i, i+65);
 
 
-  // addEdge(graph, 3, 5);
-  // addEdge(graph, 3, 7);
-  // addEdge(graph, 8, 5);
+  addEdge(graph, 0, 1);   // A-B
+  addEdge(graph, 0, 3);   // A-D
+  addEdge(graph, 0, 5);   // A-F
+  addEdge(graph, 0, 7);   // A-H
+  addEdge(graph, 1, 2);   // B-C
+  addEdge(graph, 1, 17);  // B-R
+  addEdge(graph, 1, 9);   // B-J
+  addEdge(graph, 1, 8);   // B-I
+  addEdge(graph, 2, 16);  // C-R
+  addEdge(graph, 2, 17);  // C-Q
+  addEdge(graph, 2, 3);   // C-D
+  addEdge(graph, 3, 16);  // D-Q
+  addEdge(graph, 3, 15);  // D-P
+  addEdge(graph, 3, 4);   // D-E
+  addEdge(graph, 4, 15);  // E-P
+  addEdge(graph, 4, 13);  // E-N
+  addEdge(graph, 4, 5);   // E-F
+  addEdge(graph, 5, 13);  // F-N
+  addEdge(graph, 5, 12);  // F-M
+  addEdge(graph, 5, 6);   // F-G
+  addEdge(graph, 6, 12);  // G-M
+  addEdge(graph, 6, 11);  // G-L
+  addEdge(graph, 6, 7);   // G-H
+  addEdge(graph, 7, 11);  // H-L
+  addEdge(graph, 7, 10);  // H-K
+  addEdge(graph, 7, 8);   // H-I
+  addEdge(graph, 8, 10);  // I-K
+  addEdge(graph, 8, 9);   // I-J
+
+
+  listNeighbors(graph,3);
+  // printVertex(graph);
+   findAllWay(graph, stack, 6, 12);
+
+  // push(stack, 0);
+  // push(stack, 12);
+  // push(stack, 15);
+  // printWay(graph, stack);
+  // printf("\n cekilen: %d \n",pull(stack));
+  // printWay(graph,stack);
+  // printf("\n");
+
+
+
   //
-  // listNeighbors(graph,3);
   // listNeighbors(graph,5);
 
-  struct Queue* queue = createQueue(10);
-  enqueue(queue, graph->array[0].vertex);
-  enqueue(queue, graph->array[3].vertex);
-  enqueue(queue, graph->array[1].vertex);
-  printQueue(queue);
-  dequeue(queue);
-  printQueue(queue);
-  
+  // struct Queue* queue = createQueue();
+  // enqueue(queue, graph->array[0].vertex);
+  // enqueue(queue, graph->array[3].vertex);
+  // enqueue(queue, graph->array[1].vertex);
+  // printQueue(queue);
+  // dequeue(queue);
+  // printQueue(queue);
 
 
+  // bfs(graph, 6);
   // printVertex(graph);
 
 }
